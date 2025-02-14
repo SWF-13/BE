49a2b1d0001ac75bc3f414b3dc782ebd3d7e8f0d
kim-kwan-il <david5452@naver.com>
2025-02-15 03:07:45 +0900

    스크랩 테이블 생성

--- src/main/java/com/example/ggj_be/domain/scrap/Scrap.java
+++ src/main/java/com/example/ggj_be/domain/scrap/Scrap.java
@@ -1,4 +1,4 @@
-package com.example.ggj_be.domain.comment;
+package com.example.ggj_be.domain.scrap;
 
 
 import com.example.ggj_be.domain.board.Board;
@@ -13,12 +13,12 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "comment_tb")
-public class Comment {
+@Table(name = "scrap_tb")
+public class Scrap {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "seq")
-    private Long id;  // 댓글 고유번호
+    private Long id; //스크랩 고유번호
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_seq", nullable = false)
@@ -28,16 +28,6 @@ public class Comment {
     @JoinColumn(name = "board_seq", nullable = false)
     private Board board;  // 게시판 정보 (외래키)
 
-    @Column(name = "reply_content", nullable = false, length = 255)
-    private String content;  // 댓글 내용
-
-    @Column(name = "acc_at")
-    private LocalDateTime accAt;  // 채택 시간 (NULL 허용)
-
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;  // 댓글 생성 시간
-
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;  // 댓글 수정 시간
-
 }
