f8a7164bbb2f571b781593055c16fc3371c2f55e
kim-kwan-il <david5452@naver.com>
2025-03-01 02:19:34 +0900

    jwt 엔트리 포인트 로그 수정

--- src/main/java/com/example/ggj_be/global/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/ggj_be/global/jwt/JwtAuthenticationEntryPoint.java
@@ -24,11 +24,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         ErrorStatus exception = (ErrorStatus) request.getAttribute("exception");
         log.info("===================== EntryPoint - Exception Control : " + request.getAttribute(
                 "exception"));
-        if (exception == null) {
-            log.error("JWT 관련 오류 정보가 없습니다.");
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 관련 오류 정보가 없습니다.");
-            return;
-        }
 
         if (exception.equals(ErrorStatus._JWT_NOT_FOUND)) {
             exceptionHandler(response, ErrorStatus._JWT_NOT_FOUND,
