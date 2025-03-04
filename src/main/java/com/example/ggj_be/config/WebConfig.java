432180a47d12b08a57524d7cd44fb8a944b99f22
seungsoo <PC@192.168.219.100>
2025-03-04 14:57:11 +0900

    이미지경로 변경

--- src/main/java/com/example/ggj_be/config/WebConfig.java
+++ src/main/java/com/example/ggj_be/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.ggj_be.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,11 +11,13 @@ import java.nio.file.Paths;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final String UPLOAD_DIR = Paths.get("").toAbsolutePath().toString()+ File.separator+"src"+File.separator+"uploads"+File.separator;
+    // private static final String UPLOAD_DIR = Paths.get("").toAbsolutePath().toString()+ File.separator+"src"+File.separator+"uploads"+File.separator;
+    @Value("${upload-dir}")
+    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/potoUrl/**")
-                .addResourceLocations("file:" + UPLOAD_DIR);
+                .addResourceLocations("file:" + uploadDir);
     }
 }
