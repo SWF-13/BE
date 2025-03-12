a264619a731c7d83fce8b3a4228e64f28e5f7a30
seungsoo <PC@192.168.219.100>
2025-03-12 21:37:33 +0900

    인출기능 오류확인용

--- src/main/java/com/example/ggj_be/domain/point/service/PointService.java
+++ src/main/java/com/example/ggj_be/domain/point/service/PointService.java
@@ -13,7 +13,7 @@ public interface PointService {
     List<Point> getUserList(PointType pointType);
     List<Member> getAllUserList();
     Boolean createPoint(Member member, PointCreateRequest request);
-    Boolean updatePoint(Long userId, PointUpateRequest request);
+    Boolean updatePoint(PointUpateRequest request);
     Boolean updateComment(CommentRequest request);
     List<Point>getPointList(Member member, String period);
 
